525f46f012038744f07c0a8d6c7617854ee70a9f
arieleden <ariededen@gmail.com>
2024-09-01 22:41:49 +0200

    Fixed comment error Shirel Branch.

--- app/src/main/java/com/ariel/sculptingexampel23/MainActivity.kt
+++ app/src/main/java/com/ariel/sculptingexampel23/MainActivity.kt
@@ -10,7 +10,11 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.ariel.sculptingexampel23.ui.theme.SculptingExampel23Theme
 
 class MainActivity : ComponentActivity() {
@@ -34,14 +38,9 @@ class MainActivity : ComponentActivity() {
 fun Greeting(name: String, modifier: Modifier = Modifier) {
     Text(
         text = "Hello $name!",
-        modifier = modifier
+        modifier = modifier.padding(16.dp),
+        color = Color.Green,
+        fontSize = 20.sp,
+        fontWeight = FontWeight.Bold
     )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    SculptingExampel23Theme {
-        Greeting("Android")
-    }
 }
